c024f14512c1446a65d3a19dd2abcf27f95762ae
Surya Elavazhagan <surya@localhost.localdomain>
2021-04-25 22:13:10 +0530

    deploy

---
+++ precache-manifest.f3432944d74949e7beea3f9ab166edf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe2bafebe32a0aa6d8f",
+    "url": "/phuzion/css/app.3e2c0181.css"
+  },
+  {
+    "revision": "b807a4d8596ef8ec205a",
+    "url": "/phuzion/css/chunk-vendors.4585760b.css"
+  },
+  {
+    "revision": "4f5758632065e275255c729a9c4bbe2d",
+    "url": "/phuzion/img/logo.4f575863.png"
+  },
+  {
+    "revision": "5ed2701d27933a26c6614543a282f15d",
+    "url": "/phuzion/index.html"
+  },
+  {
+    "revision": "dfe2bafebe32a0aa6d8f",
+    "url": "/phuzion/js/app.8ad750e7.js"
+  },
+  {
+    "revision": "b807a4d8596ef8ec205a",
+    "url": "/phuzion/js/chunk-vendors.a8482d62.js"
+  },
+  {
+    "revision": "b775267395e97200567d279159132c6c",
+    "url": "/phuzion/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/phuzion/robots.txt"
+  }
+]);
